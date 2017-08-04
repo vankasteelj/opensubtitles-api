1f0dc59db4be9a475f69ded394fc31dcd19b400c
jcrisolini <jcrisolini@consensusgroup.net>
2017-08-04 19:29:32 -0300

    Best Score Logic, matchTags rollback

--- tests.js
+++ tests.js
@@ -1,8 +1,19 @@
 const OS = require('./index.js')
 const UA = 'OSTestUserAgentTemp'
-const imdb = '0898266', show = 'The Big Bang Theory', s = '01', ep = '01'
-
 let os, test
+const testSearch = 0
+const searchs = [
+    // 0 imdb 
+    { imdbid: '0898266', show: 'The Big Bang Theory', season: '01', episode: '01', limit: 'all' },
+    // 1 imdb + filename (typical popcorn-time query)
+    { imdbid: '0898266', season: '01', episode: '01', filename: 'The.Big.Bang.Theory.S01E01.HDTV.XviD-XOR.mkv', limit: 'all' },
+    // 2 tag 
+    { filename: 'The Big Bang Theory S01E01.mkv', limit: 'all' },
+    // 3 hash (put a real path)
+    { path: "D:/TvShows/The Big Bang Theory/Season 1/The Big Bang Theory S01E01.mkv", limit: 'all' },
+    // 4 hash + imdb (put a real path)
+    { path: "D:/TvShows/The Big Bang Theory/Season 1/The Big Bang Theory S01E01.mkv", imdbid: '0898266', season: '01', episode: '01', limit: 'all' },
+]
 
 test = 'http client'
 os = new OS({
@@ -25,12 +36,9 @@ os.api.ServerInfo().then(() => {
 
     test = 'search'
     console.time(test)
-    return os.search({
-        season: s,
-        episode: ep,
-        imdbid: imdb,
-        limit: 'all'
-    })
+    //return seachs.map(os.search)
+
+    return os.search(searchs[testSearch])
 }).then(() => {
     console.timeEnd(test)
     console.log('Passed test.')
